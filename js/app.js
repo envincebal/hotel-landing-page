71bdb6fe0e920023d019b377a90b765d2a3affd6
envincebal <vincentyan1@gmail.com>
2021-08-20 02:08:17 -0700

    Added smooth scrolling and responsive design.

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// Opacity scroll effect
+$(window).on("scroll", function () {
+  if (window.scrollY > 810) {
+    $("#navbar").css("opacity", "0.9");
+  } else {
+    $("#navbar").css("opacity", "1");
+  }
+});
+
+// Smooth Scrolling
+
+$("#navbar a, .btn").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate({
+      scrollTop: $(hash).offset().top - 58
+    }, 800);
+  }
+});
